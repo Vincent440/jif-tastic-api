d35965969cd2cbb4682972b40169dab1fa327405
Vince216 <vinceshury@gmail.com>
2019-03-06 15:03:50 -0500

    linked port and cleaned up psuedocode

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # jif-tastic-api
 using the GIPHY API to make a dynamic web page that populates with gifs. GIPHY API JavaScript and jQuery
+link to live file of game [https://vincent440.github.io/jif-tastic-api/]
+link to my portfolio page [https://vincent440.github.io]
--- assets/js/app.js
+++ assets/js/app.js
@@ -2,8 +2,10 @@
 /* eslint-disable no-console */
 /*eslint-env browser*/
 /*global $*/
+//--------------------------------------------------------------------------------------------------------------
+//link to portfolio page https://vincent440.github.io/portfolio.html
 
-//-------------------Giphy API WEEK 6 HW----------------------Global variables--------------------------------------
+//-------------------Giphy API WEEK 6 HW----------------------Global variables----------------------------------
 
 //Starting topics array based around Space/astronomy styles
 //Push user input from Textbox to this array then button placement Function call to include the new string added to array. 
@@ -24,11 +26,7 @@ var queryURL = "https://api.giphy.com/v1/gifs/search?q="+queryUserInput+apiLimit
 
 //console log query url to test call & check for errors
 console.log(queryURL);
-
-//-----------------------------------------FUNCTION CREATION--------------------------------------------------------
-
- 
-
+//-----------------------------------------FUNCTION CREATION-----------------------------------------------------
 function topicButtonsGen() {
     //function to generate buttons with the topics array as the data Value
     //this function will be called when the page loads , and on the click event
@@ -37,9 +35,7 @@ function topicButtonsGen() {
     //clear the DIV containing the current ApiTopic buttons to prevent duplicates 
 
     //  Initiate variable to build APIBUTTONS var apiButtons = $("<button>");
-
     //  inside for loop  (loop through each index of the topic array )      {
-
     //        .attr("data-topic",String pulled from current index of the array)
     //  add attribute equal to the String of the current index in the array for each button to be able to grab this value to make the api request
     //       .addClass(apiCallButton) to the buttons
@@ -58,23 +54,15 @@ function imageCreation(){
     //add attributes to the images if needed here
     //add a class to the gifs for either styling or click function if needed to start/stop animation
     //DOM push the gifs to the page inside the DIV created to hold them
-
 }
-
-
-
-//----DOCUMENT READY-----------------------FUNCTION CALLS AND CLICK EVENTS-------------------------------------------
+//----DOCUMENT READY-----------------------FUNCTION CALLS AND CLICK EVENTS-----------------------------------------
 
 // Shorthand for $( document ).ready()
 $(function() {
-
     //document ready function to prevent any issues well loading JS and the page. 
     //initial call to generate the TOPICS Array buttons when the page is loaded.
-    console.log( "ready!" );
-    
-
+    console.log( "ready!" );    
     $("#textButton").on( "click", function() {  
-
         
         //Click event for submit button
         //  event prevent default to stop page from refreshing on clicks
@@ -84,12 +72,8 @@ $(function() {
         //console.log is your friend
         event.preventDefault();
         console.log("text button clicked!");
-
         var buttonData = $("#userButtonText").val().trim();
-
-        console.log(buttonData);
-        
-
+        console.log(buttonData);     
       $("#userButtonText").val("");
 
         console.log("User entered: "+ buttonData +" ;in the textbox" );
@@ -99,27 +83,15 @@ $(function() {
     });
     //Click event for API Call buttons class
     $(".apiCallButton").on( "click", function() {  
-
-        
+       
         //      use the Value from the button clicked 
         //      attach to an AJAX call with the value in the search term
-
         //      AJAX CALL
         //and THEN function inside click event
-
-        //      
-
         
     });
-
-
-
     //?? click event for GIF images to start/stop them not sure of the functionality of the gifs retrived from api
     //how I can maybe make the gifs start/stop
     //      on the click event of the gif class have the image src switch from image_Still url to the playable gif_url retrived from the response. 
     //      will NEED to read more on the DOCUMENTATION/ test out gifs on page when they are succesfully placed into html
-  
-
-});
-
-
+  });
