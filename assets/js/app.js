4e7d55441a9159a92d4f909a70c61e4f971ca258
Vince216 <vinceshury@gmail.com>
2019-07-17 09:05:08 -0400

    Updated JavaScript to ES6 const let and arrow func

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,32 +1,33 @@
 /*eslint-env browser*/
 /*global $*/
-var topics = ["Mercury", "Venus", "Earth", "Mars", "Jupiter", "Saturn", "Uranus", "Neptune", "Lunar", "Sun", "Comets", "Asteroids", "Space", "Stars", "Galaxy", "Cosmos", "Astronomy", "Universe", "Black Holes"]; // Starting topics array based around Space/astronomy styles
-var apiKey = "&api_key=aDWTP2Hv0BWis8vPpDBeKqrdD6aBRF6W"; // API key used to retrieve data from the servers.
-var limitValue = "12"; // Request limit set to 12, bonus = add another user input to adjust the limit accordingly to whatever value is selected.
-var queryURL = "https://api.giphy.com/v1/gifs/search?rating=pg-13&limit=" + limitValue + apiKey + "&q=";
+const topics = ["Mercury", "Venus", "Earth", "Mars", "Jupiter", "Saturn", "Uranus", "Neptune", "Lunar", "Sun", "Comets", "Asteroids", "Space", "Stars", "Galaxy", "Cosmos", "Astronomy", "Universe", "Black Holes"]; // Starting topics array based around Space/astronomy styles
+const apiKey = "&api_key=aDWTP2Hv0BWis8vPpDBeKqrdD6aBRF6W"; // API key used to retrieve data from the servers.
+const limitValue = "12"; // Request limit set to 12, bonus = add another user input to adjust the limit accordingly to whatever value is selected.
+let queryURL = "https://api.giphy.com/v1/gifs/search?rating=pg-13&limit=" + limitValue + apiKey + "&q=";
 // query url with rating hard coded in, with variable limit and search parameters.
-function topicButtonsGen() {
+const topicButtonsGen = () => {
   $("#button-placement").empty(); // clear the DIV containing the current ApiTopic buttons to prevent duplicates
-  for (var i = 0; i < topics.length; i++) {
-    var apiButtons = $("<button>"); //  Initiate variable to build APIBUTTONS
+  for (let i = 0; i < topics.length; i++) {
+    let apiButtons = $("<button>"); //  Initiate variable to build APIBUTTONS
     apiButtons
       .attr("type", "button") // apply required attributes to buttons
       .attr("data-topicvalue", topics[i])
       .addClass("btn btn btn-dark api-call-buttons m-1") // add classes for bootstrap bootstrap styling and a handle for click event/custom styling
       .text(topics[i]); // places string from current index of the topics array
-    $("#button-placement").append(apiButtons); //append the values to the button-placement div each iteration of the loop
+    $("#button-placement").append(apiButtons); // append the values to the button-placement div each iteration of the loop
   }
 }
-function imageCreation(ajaxData) {
-  var gifImgData = ajaxData.data;
+
+const imageCreation = (ajaxData) => {
+  let gifImgData = ajaxData.data;
   $("#image-box").empty(); // remove previous images each new request.
-  for (var gifIndex = 0; gifIndex < gifImgData.length; gifIndex++) {
-    var gifRating = gifImgData[gifIndex].rating;
-    var fixedUrl = gifImgData[gifIndex].images.original_still.url;
-    var animatedUrl = gifImgData[gifIndex].images.original.url;
-    var imageFigure = $("<figure>");
-    var imgTag = $("<img>");
-    var figCaption = $("<figcaption>");
+  for (let gifIndex = 0; gifIndex < gifImgData.length; gifIndex++) {
+    let gifRating = gifImgData[gifIndex].rating;
+    let fixedUrl = gifImgData[gifIndex].images.original_still.url;
+    let animatedUrl = gifImgData[gifIndex].images.original.url;
+    let imageFigure = $("<figure>");
+    let imgTag = $("<img>");
+    let figCaption = $("<figcaption>");
     imageFigure
       .addClass("figure col-md-3 col-lg-4")
       .append(imgTag)
@@ -42,8 +43,9 @@ function imageCreation(ajaxData) {
     $("#image-box").prepend(imageFigure);
   }
 }
+
 function getApiCallData() {
-  var searchValue = $(this)
+  let searchValue = $(this)
     .attr("data-topicvalue")
     .trim();
   $("#gifDisplay").html("Displaying: " + "<h4 class='display-3 font-italic'>" + searchValue + " Gifs</h4>" + "Click the Images to animate them!");
@@ -53,18 +55,19 @@ function getApiCallData() {
   $.ajax({
     url: queryURL,
     type: "GET"
-  }).then(function(responseData) {
-    imageCreation(responseData);
-    return responseData;
+  }).then(res  => {
+    imageCreation(res);
+    return res;
   });
   queryURL = "https://api.giphy.com/v1/gifs/search?rating=pg-13&limit=" + limitValue + apiKey + "&q="; //RESETS QUERY URL
   $("#bottomBtn").show(); // shows scroll to top button
 }
-$(function() {
+
+$(() => {
   topicButtonsGen(); // initial call to generate the TOPICS Array buttons when the page is loaded.
   $("#textButton").on("click", function(event) {
     event.preventDefault();// need error handling to prevent any input from submitting to topics array
-    var buttonData = $("#userButtonText")
+    let buttonData = $("#userButtonText")
       .val()
       .trim();
     topics.push(buttonData);
@@ -72,12 +75,12 @@ $(function() {
     buttonData = "";
     topicButtonsGen(); // Call to the API BUTTON placement function to add to the HTML document new user input along with everything in the array previously
   });
-  $(document).on("click", ".api-call-buttons", getApiCallData); //Click event for API Call buttons class on the document
+  $(document).on("click", ".api-call-buttons", getApiCallData); // Click event for API Call buttons class on the document
   $(document).on("click", ".imgClick", function() {
     // Click event to start/stop gif animation
-    var animated = $(this).data("status");
-    var imgFix = $(this).data("fixed");
-    var imgGif = $(this).data("animated");
+    const animated = $(this).data("status");
+    const imgFix = $(this).data("fixed");
+    const imgGif = $(this).data("animated");
     if (animated == false) {
       $(this).attr("src", imgGif);
       $(this).data("status", true);
